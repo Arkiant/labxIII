93eeb8c4894a4e5704bfa0e631e5e57f260a9101
manuel <manuel.fdez.alvarez@gmail.com>
2019-11-30 18:44:43 +0100

    https

--- src/dialog/server.go
+++ src/dialog/server.go
@@ -8,5 +8,13 @@ import (
 
 func main() {
 	r := routes.NewRoutes()
+
+	/*
+			err := http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/enimada.com/fullchain.pem", "/etc/letsencrypt/live/enimada.com/privkey.pem", r)
+		        if err != nil {
+		                panic(err)
+				}
+	*/
+
 	http.ListenAndServe(":6969", r)
 }
